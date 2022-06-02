dc8aa2553c51a9d38985fb77b65b1b2a77535299
Max <All2Enable@gmail.com>
2022-06-02 14:15:25 +0300

    Changed views to function that can be easily called to know any openVPN server status, only write path Added another server status container

--- VPNsite/VPNcheck/views.py
+++ VPNsite/VPNcheck/views.py
@@ -1,28 +1,33 @@
 import requests, subprocess, time
 from django.shortcuts import render
 
-def index(request):
-    result = 0
 
+def getopenVPNserver(path):
+    url = 'https://ifconfig.io/ip'
+    r = requests.get(url)
+    process = subprocess.Popen(f"sudo openvpn --auth-nocache --config {path}", shell=True)
+    time.sleep(5)
+    r2 = requests.get(url)
+    process.kill()
+    if r.text != r2.text:
+        subprocess.run("sudo killall openvpn", shell=True)
+        result = 'Nice one!'
+    else:
+        result = 'Man, cringe...'
+    subprocess.run("sudo killall openvpn", shell=True)
+    return result
+
+
+def index(request):
+    get = 0
+    get2 = 0
 
     if (request.method == 'POST'):
-        url = 'https://ifconfig.io/ip'
-        r = requests.get(url)
+        #path to config files
         path = '/home/___/monitor.ovpn'
-        # your path to server cfg file
-        process = subprocess.Popen(f"sudo openvpn --auth-nocache --config {path}", shell=True)
-        time.sleep(5)
-        r2 = requests.get(url)
-        process.kill()
-        if r.text != r2.text:
-            subprocess.run("sudo killall openvpn", shell=True)
-            result = 'Nice one!'
+        path2 = '/home/___/venom.ovpn'
+        get = getopenVPNserver(path)
+        get2 = getopenVPNserver(path2)
 
-        else:
-            result = 'Man, cringe...'
-        subprocess.run("sudo killall openvpn", shell=True)
-
-        context = {'info': result}
-
-    context = {'info': result}
+    context = {'info': get, 'info2': get2}
     return render(request, 'vpn/index.html', context)
